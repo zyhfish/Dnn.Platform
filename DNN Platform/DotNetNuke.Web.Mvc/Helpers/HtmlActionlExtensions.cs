d476ac1fec8258fa0a841d0b33ec1a4a7f2bd318
Ben <zyhfish@163.com>
2017-10-12 11:40:38 +0800

    DNN_8577: add extensions fro Action and RenderAction.

---
+++ DNN Platform/DotNetNuke.Web.Mvc/Helpers/HtmlActionlExtensions.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) DNN Software. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace DotNetNuke.Web.Mvc.Helpers
+{
+    /// <summary>
+    /// Represents the functionality to render a partial view as an HTML-encoded string.
+    /// </summary>
+    public static class HtmlActionExtensions
+    {
+        public static MvcHtmlString Action(this DnnHtmlHelper helper, string actionName)
+        {
+            return helper.HtmlHelper.Action(actionName);
+        }
+
+        public static MvcHtmlString Action(this DnnHtmlHelper helper, string actionName, object routeValues)
+        {
+            return helper.HtmlHelper.Action(actionName, routeValues);
+        }
+
+        public static MvcHtmlString Action(this DnnHtmlHelper helper, string actionName, RouteValueDictionary routeValues)
+        {
+            return helper.HtmlHelper.Action(actionName, routeValues);
+        }
+
+        public static MvcHtmlString Action(this DnnHtmlHelper helper, string actionName, string controllerName)
+        {
+            return helper.HtmlHelper.Action(actionName, controllerName);
+        }
+
+        public static MvcHtmlString Action(this DnnHtmlHelper helper, string actionName, string controllerName, object routeValues)
+        {
+            return helper.HtmlHelper.Action(actionName, controllerName, routeValues);
+        }
+
+        public static MvcHtmlString Action(this DnnHtmlHelper helper, string actionName, string controllerName, RouteValueDictionary routeValues)
+        {
+            return helper.HtmlHelper.Action(actionName, controllerName, routeValues);
+        }
+    }
+}
---
+++ DNN Platform/DotNetNuke.Web.Mvc/Helpers/HtmlRenderActionlExtensions.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) DNN Software. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace DotNetNuke.Web.Mvc.Helpers
+{
+    /// <summary>
+    /// Represents the functionality to render a partial view as an HTML-encoded string.
+    /// </summary>
+    public static class HtmlRenderActionExtensions
+    {
+        public static void RenderAction(this DnnHtmlHelper helper, string actionName)
+        {
+            helper.HtmlHelper.RenderAction(actionName);
+        }
+
+        public static void RenderAction(this DnnHtmlHelper helper, string actionName, object routeValues)
+        {
+            helper.HtmlHelper.RenderAction(actionName, routeValues);
+        }
+
+        public static void RenderAction(this DnnHtmlHelper helper, string actionName, RouteValueDictionary routeValues)
+        {
+            helper.HtmlHelper.RenderAction(actionName, routeValues);
+        }
+
+        public static void RenderAction(this DnnHtmlHelper helper, string actionName, string controllerName)
+        {
+            helper.HtmlHelper.RenderAction(actionName, controllerName);
+        }
+
+        public static void RenderAction(this DnnHtmlHelper helper, string actionName, string controllerName, object routeValues)
+        {
+            helper.HtmlHelper.RenderAction(actionName, controllerName, routeValues);
+        }
+
+        public static void RenderAction(this DnnHtmlHelper helper, string actionName, string controllerName, RouteValueDictionary routeValues)
+        {
+            helper.HtmlHelper.RenderAction(actionName, controllerName, routeValues);
+        }
+    }
+}
